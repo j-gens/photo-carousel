882a9fce8265a9ba4211bf70601e343df83029c3
j-gens <jgens2@gmail.com>
2019-10-10 16:22:23 -0700

    Add a callback input to save function

--- database/index.js
+++ database/index.js
@@ -12,3 +12,25 @@ let carouselSchema = new mongoose.Schema({
 let Carousel = mongoose.model('Carousel', carouselSchema);
 
 
+const save = (data, callback) => {
+
+  for (let i = 0; i < data.length; i++) {
+    let newImg = new Carousel ({
+      _id: data[i]._id,
+      small_url: data[i].small_url,
+      large_url: data[i].large_url,
+      movie: {id: data[i].movie.id, title: data[i].movie.title}
+    })
+
+    newImg.save((err, data) => {
+      if (err) {
+        callback(err);
+      } else {
+        callback(data);
+      }
+    });
+  }
+};
+
+
+module.exports.save = save;
