0ad9d4db41b91dcb8cea2b06ebdd328deb7e670c
j-gens <jgens2@gmail.com>
2019-10-10 16:14:58 -0700

    Create data to seed database

---
+++ database/seed.js
@@ -0,0 +1,205 @@
+const db = require('./index.js');
+
+let dummyData = [
+  {
+    "_id": 310101,
+    "small_url": "https://hrr41-fec-krillin-imgs.s3-us-west-1.amazonaws.com/ph-thumb.gif",
+    "large_url": "https://hrr41-fec-krillin-imgs.s3-us-west-1.amazonaws.com/ph-horiz.jpg",
+    "movie": {"id": 210001, "title": "Frozen"}
+  },
+  {
+    "_id": 310102,
+    "small_url": "https://hrr41-fec-krillin-imgs.s3-us-west-1.amazonaws.com/ph-thumb.gif",
+    "large_url": "https://hrr41-fec-krillin-imgs.s3-us-west-1.amazonaws.com/ph-vert.jpg",
+    "movie": {"id": 210001, "title": "Frozen"}
+  },
+  {
+    "_id": 310103,
+    "small_url": "https://hrr41-fec-krillin-imgs.s3-us-west-1.amazonaws.com/ph-thumb.gif",
+    "large_url": "https://hrr41-fec-krillin-imgs.s3-us-west-1.amazonaws.com/ph-horiz.jpg",
+    "movie": {"id": 210001, "title": "Frozen"}
+  },
+  {
+    "_id": 310201,
+    "small_url": "https://hrr41-fec-krillin-imgs.s3-us-west-1.amazonaws.com/ph-thumb.gif",
+    "large_url": "https://hrr41-fec-krillin-imgs.s3-us-west-1.amazonaws.com/ph-horiz.jpg",
+    "movie": {"id": 210002, "title": "Lion King"}
+  },
+  {
+    "_id": 310202,
+    "small_url": "https://hrr41-fec-krillin-imgs.s3-us-west-1.amazonaws.com/ph-thumb.gif",
+    "large_url": "https://hrr41-fec-krillin-imgs.s3-us-west-1.amazonaws.com/ph-vert.jpg",
+    "movie": {"id": 210002, "title": "Lion King"}
+  },
+  {
+    "_id": 310203,
+    "small_url": "https://hrr41-fec-krillin-imgs.s3-us-west-1.amazonaws.com/ph-thumb.gif",
+    "large_url": "https://hrr41-fec-krillin-imgs.s3-us-west-1.amazonaws.com/ph-horiz.jpg",
+    "movie": {"id": 210002, "title": "Lion King"}
+  },
+  {
+    "_id": 310204,
+    "small_url": "https://hrr41-fec-krillin-imgs.s3-us-west-1.amazonaws.com/ph-thumb.gif",
+    "large_url": "https://hrr41-fec-krillin-imgs.s3-us-west-1.amazonaws.com/ph-vert.jpg",
+    "movie": {"id": 210002, "title": "Lion King"}
+  },
+  {
+    "_id": 310205,
+    "small_url": "https://hrr41-fec-krillin-imgs.s3-us-west-1.amazonaws.com/ph-thumb.gif",
+    "large_url": "https://hrr41-fec-krillin-imgs.s3-us-west-1.amazonaws.com/ph-horiz.jpg",
+    "movie": {"id": 210002, "title": "Lion King"}
+  },
+  {
+    "_id": 310206,
+    "small_url": "https://hrr41-fec-krillin-imgs.s3-us-west-1.amazonaws.com/ph-thumb.gif",
+    "large_url": "https://hrr41-fec-krillin-imgs.s3-us-west-1.amazonaws.com/ph-vert.jpg",
+    "movie": {"id": 210002, "title": "Lion King"}
+  },
+  {
+    "_id": 310207,
+    "small_url": "https://hrr41-fec-krillin-imgs.s3-us-west-1.amazonaws.com/ph-thumb.gif",
+    "large_url": "https://hrr41-fec-krillin-imgs.s3-us-west-1.amazonaws.com/ph-horiz.jpg",
+    "movie": {"id": 210002, "title": "Lion King"}
+  },
+  {
+    "_id": 310208,
+    "small_url": "https://hrr41-fec-krillin-imgs.s3-us-west-1.amazonaws.com/ph-thumb.gif",
+    "large_url": "https://hrr41-fec-krillin-imgs.s3-us-west-1.amazonaws.com/ph-vert.jpg",
+    "movie": {"id": 210002, "title": "Lion King"}
+  },
+  {
+    "_id": 310209,
+    "small_url": "https://hrr41-fec-krillin-imgs.s3-us-west-1.amazonaws.com/ph-thumb.gif",
+    "large_url": "https://hrr41-fec-krillin-imgs.s3-us-west-1.amazonaws.com/ph-horiz.jpg",
+    "movie": {"id": 210002, "title": "Lion King"}
+  },
+  {
+    "_id": 310210,
+    "small_url": "https://hrr41-fec-krillin-imgs.s3-us-west-1.amazonaws.com/ph-thumb.gif",
+    "large_url": "https://hrr41-fec-krillin-imgs.s3-us-west-1.amazonaws.com/ph-vert.jpg",
+    "movie": {"id": 210002, "title": "Lion King"}
+  },
+  {
+    "_id": 310211,
+    "small_url": "https://hrr41-fec-krillin-imgs.s3-us-west-1.amazonaws.com/ph-thumb.gif",
+    "large_url": "https://hrr41-fec-krillin-imgs.s3-us-west-1.amazonaws.com/ph-horiz.jpg",
+    "movie": {"id": 210002, "title": "Lion King"}
+  },
+  {
+    "_id": 310212,
+    "small_url": "https://hrr41-fec-krillin-imgs.s3-us-west-1.amazonaws.com/ph-thumb.gif",
+    "large_url": "https://hrr41-fec-krillin-imgs.s3-us-west-1.amazonaws.com/ph-vert.jpg",
+    "movie": {"id": 210002, "title": "Lion King"}
+  },
+  {
+    "_id": 310301,
+    "small_url": "https://hrr41-fec-krillin-imgs.s3-us-west-1.amazonaws.com/ph-thumb.gif",
+    "large_url": "https://hrr41-fec-krillin-imgs.s3-us-west-1.amazonaws.com/ph-vert.jpg",
+    "movie": {"id": 210003, "title": "Robin Hood"}
+  },
+  {
+    "_id": 310302,
+    "small_url": "https://hrr41-fec-krillin-imgs.s3-us-west-1.amazonaws.com/ph-thumb.gif",
+    "large_url": "https://hrr41-fec-krillin-imgs.s3-us-west-1.amazonaws.com/ph-vert.jpg",
+    "movie": {"id": 210003, "title": "Robin Hood"}
+  },
+  {
+    "_id": 310303,
+    "small_url": "https://hrr41-fec-krillin-imgs.s3-us-west-1.amazonaws.com/ph-thumb.gif",
+    "large_url": "https://hrr41-fec-krillin-imgs.s3-us-west-1.amazonaws.com/ph-vert.jpg",
+    "movie": {"id": 210003, "title": "Robin Hood"}
+  },
+  {
+    "_id": 310304,
+    "small_url": "https://hrr41-fec-krillin-imgs.s3-us-west-1.amazonaws.com/ph-thumb.gif",
+    "large_url": "https://hrr41-fec-krillin-imgs.s3-us-west-1.amazonaws.com/ph-vert.jpg",
+    "movie": {"id": 210003, "title": "Robin Hood"}
+  },
+  {
+    "_id": 310305,
+    "small_url": "https://hrr41-fec-krillin-imgs.s3-us-west-1.amazonaws.com/ph-thumb.gif",
+    "large_url": "https://hrr41-fec-krillin-imgs.s3-us-west-1.amazonaws.com/ph-vert.jpg",
+    "movie": {"id": 210003, "title": "Robin Hood"}
+  },
+  {
+    "_id": 310306,
+    "small_url": "https://hrr41-fec-krillin-imgs.s3-us-west-1.amazonaws.com/ph-thumb.gif",
+    "large_url": "https://hrr41-fec-krillin-imgs.s3-us-west-1.amazonaws.com/ph-vert.jpg",
+    "movie": {"id": 210003, "title": "Robin Hood"}
+  },
+  {
+    "_id": 310307,
+    "small_url": "https://hrr41-fec-krillin-imgs.s3-us-west-1.amazonaws.com/ph-thumb.gif",
+    "large_url": "https://hrr41-fec-krillin-imgs.s3-us-west-1.amazonaws.com/ph-vert.jpg",
+    "movie": {"id": 210003, "title": "Robin Hood"}
+  },
+  {
+    "_id": 310308,
+    "small_url": "https://hrr41-fec-krillin-imgs.s3-us-west-1.amazonaws.com/ph-thumb.gif",
+    "large_url": "https://hrr41-fec-krillin-imgs.s3-us-west-1.amazonaws.com/ph-vert.jpg",
+    "movie": {"id": 210003, "title": "Robin Hood"}
+  },
+  {
+    "_id": 310401,
+    "small_url": "https://hrr41-fec-krillin-imgs.s3-us-west-1.amazonaws.com/ph-thumb.gif",
+    "large_url": "https://hrr41-fec-krillin-imgs.s3-us-west-1.amazonaws.com/ph-horiz.jpg",
+    "movie": {"id": 210004, "title": "Beauty and the Beast"}
+  },
+  {
+    "_id": 310402,
+    "small_url": "https://hrr41-fec-krillin-imgs.s3-us-west-1.amazonaws.com/ph-thumb.gif",
+    "large_url": "https://hrr41-fec-krillin-imgs.s3-us-west-1.amazonaws.com/ph-horiz.jpg",
+    "movie": {"id": 210004, "title": "Beauty and the Beast"}
+  },
+  {
+    "_id": 310403,
+    "small_url": "https://hrr41-fec-krillin-imgs.s3-us-west-1.amazonaws.com/ph-thumb.gif",
+    "large_url": "https://hrr41-fec-krillin-imgs.s3-us-west-1.amazonaws.com/ph-horiz.jpg",
+    "movie": {"id": 210004, "title": "Beauty and the Beast"}
+  },
+  {
+    "_id": 310404,
+    "small_url": "https://hrr41-fec-krillin-imgs.s3-us-west-1.amazonaws.com/ph-thumb.gif",
+    "large_url": "https://hrr41-fec-krillin-imgs.s3-us-west-1.amazonaws.com/ph-horiz.jpg",
+    "movie": {"id": 210004, "title": "Beauty and the Beast"}
+  },
+  {
+    "_id": 310501,
+    "small_url": "https://hrr41-fec-krillin-imgs.s3-us-west-1.amazonaws.com/ph-thumb.gif",
+    "large_url": "https://hrr41-fec-krillin-imgs.s3-us-west-1.amazonaws.com/ph-horiz.jpg",
+    "movie": {"id": 210005, "title": "Big Hero 6"}
+  },
+  {
+    "_id": 310502,
+    "small_url": "https://hrr41-fec-krillin-imgs.s3-us-west-1.amazonaws.com/ph-thumb.gif",
+    "large_url": "https://hrr41-fec-krillin-imgs.s3-us-west-1.amazonaws.com/ph-horiz.jpg",
+    "movie": {"id": 210005, "title": "Big Hero 6"}
+  },
+  {
+    "_id": 310503,
+    "small_url": "https://hrr41-fec-krillin-imgs.s3-us-west-1.amazonaws.com/ph-thumb.gif",
+    "large_url": "https://hrr41-fec-krillin-imgs.s3-us-west-1.amazonaws.com/ph-vert.jpg",
+    "movie": {"id": 210005, "title": "Big Hero 6"}
+  },
+  {
+    "_id": 310504,
+    "small_url": "https://hrr41-fec-krillin-imgs.s3-us-west-1.amazonaws.com/ph-thumb.gif",
+    "large_url": "https://hrr41-fec-krillin-imgs.s3-us-west-1.amazonaws.com/ph-horiz.jpg",
+    "movie": {"id": 210005, "title": "Big Hero 6"}
+  },
+  {
+    "_id": 310505,
+    "small_url": "https://hrr41-fec-krillin-imgs.s3-us-west-1.amazonaws.com/ph-thumb.gif",
+    "large_url": "https://hrr41-fec-krillin-imgs.s3-us-west-1.amazonaws.com/ph-horiz.jpg",
+    "movie": {"id": 210005, "title": "Big Hero 6"}
+  },
+  {
+    "_id": 310506,
+    "small_url": "https://hrr41-fec-krillin-imgs.s3-us-west-1.amazonaws.com/ph-thumb.gif",
+    "large_url": "https://hrr41-fec-krillin-imgs.s3-us-west-1.amazonaws.com/ph-vert.jpg",
+    "movie": {"id": 210005, "title": "Big Hero 6"}
+  }
+];
+
+
+db.save(dummyData);
