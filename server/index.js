29031321be70d7262d8568a174fe380e25be7e6e
j-gens <jgens2@gmail.com>
2019-10-11 07:36:55 -0700

    Create GET route for initial load of photo carousel

--- server/index.js
+++ server/index.js
@@ -1,12 +1,19 @@
 const express = require('express');
+const db = require('../database/index.js')
+
 const app = express();
-
-
 app.use(express.static('public'));
 
 
-
-
+app.get('/api/:movieId/imgs', (req, res) => {
+  db.Carousel.find({ 'movie.id': req.params.movieId }, (err, results) => {
+    if (err) {
+      res.send(err);
+    } else {
+      res.send(results);
+    }
+  });
+});
 
 
 //set up for future deployment:
