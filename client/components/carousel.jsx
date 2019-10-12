d9abe62568a48f5aa3db7cfa4a8167f756eebb2a
j-gens <jgens2@gmail.com>
2019-10-12 07:28:40 -0700

    Create initial react component skeleton

---
+++ client/components/carousel.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Thumbnail from './thumbnail.jsx';
+
+
+const Carousel = (props) => (
+  <div>
+    <Thumbnail />
+  </div>
+)
+
+
+export default Carousel;
---
+++ client/components/thumbnail.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Thumbnail = (props) => (
+  <div>Hello I am an image</div>
+)
+
+
+export default Thumbnail;
