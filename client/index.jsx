34f351358606c9940bf7998f266caa446c59789d
j-gens <jgens2@gmail.com>
2019-10-12 07:29:10 -0700

    Connect app component to skeleton components

--- client/index.jsx
+++ client/index.jsx
@@ -1,21 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import Carousel from './components/carousel.jsx';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      carousel: []
     }
+  }
+
+  componentDidMount() {
+    //fetch function here
   }
 
   render() {
     return (
       <div>
-        Image Carousel
+        <Carousel />
       </div>
-    )
+    );
   }
 }
 
