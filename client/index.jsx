d593878109a6e866ddf170854bc7f16d9307f14b
j-gens <jgens2@gmail.com>
2019-10-13 16:48:14 -0700

    Final naming of div tags within app

--- client/index.jsx
+++ client/index.jsx
@@ -5,6 +5,8 @@ import Carousel from './components/carousel.jsx';
 class App extends React.Component {
   constructor(props) {
     super(props);
+    //state is currently hardcoded while setting up divs
+    //will change this in next pull request
     this.state = {
       carousel: [{
         small_url: 'https://hrr41-fec-krillin-imgs.s3-us-west-1.amazonaws.com/small0.jpg'
@@ -22,6 +24,8 @@ class App extends React.Component {
     //fetch function here
   }
 
+  //will add dynamic navigation bar in future pull request
+  // @ symbols will be replaced with arrow images
   render() {
     return (
       <div className="carousel-body">
@@ -33,7 +37,11 @@ class App extends React.Component {
             navigation bar here
           </div>
         </div>
-        <Carousel carousel={this.state.carousel} />
+        <div className="carousel-bin">
+          <button className="carousel-left"> @ </button>
+          <Carousel carousel={this.state.carousel} />
+          <button className="carousel-right"> @ </button>
+        </div>
         <div className="carousel-viewAll">
           <a href="http://www.google.com">View All Photos ({this.state.carousel.length})</a>
         </div>
