34c3aab0bff449a66eb16f60e9fdf2c9af427aa5
j-gens <jgens2@gmail.com>
2019-10-14 12:11:37 -0700

    Complete function to break image list into groups of fours (nested arrays)

--- client/index.jsx
+++ client/index.jsx
@@ -10,10 +10,44 @@ class App extends React.Component {
     this.state = {
       currentMovie: '',
       carousel: [],
+      carouselByFours: [],
       currentFour: []
     }
 
     this.fetch = this.fetch.bind(this);
+    this.groupImagesByFours = this.groupImagesByFours.bind(this);
+  }
+
+  //this function breaks the images into an array of arrays
+    // inner arrays have four images each in them
+    // if movie has < 4 images total then add in 'blank' image as placeholder
+  groupImagesByFours(carousel) {
+    var count = carousel.length / 4;
+    var selectedFour = [];
+    var allGroupsOfFour = [];
+    var genericImage= {"_id": "placeholder",
+      "small_url": "https://hrr41-fec-krillin-imgs.s3-us-west-1.amazonaws.com/ph-thumb.gif"};
+
+    if (carousel.length < 4) {
+      selectedFour = carousel.slice();
+      while (selectedFour.length < 4) {
+        selectedFour.push(genericImage);
+      }
+      allGroupsOfFour.push(selectedFour);
+    }
+
+    var i = 0;
+    while (i < count) {
+      selectedFour = carousel.slice(i, i + 4);
+      if (selectedFour.length < 4) {
+        selectedFour = carousel.slice(-4);
+      }
+      allGroupsOfFour.push(selectedFour);
+      i++;
+    }
+
+    this.setState({carouselByFours: allGroupsOfFour});
+    this.setState({currentFour: this.state.carouselByFours[0]});
   }
 
   fetch(url, params) {
@@ -26,8 +60,8 @@ class App extends React.Component {
       console.log(error);
     })
     .finally(() => {
-      this.setState({currentFour: this.state.carousel.slice(0, 4),
-      currentMovie: this.state.carousel[0].movie.title})
+      this.setState({currentMovie: this.state.carousel[0].movie.title});
+      this.groupImagesByFours(this.state.carousel);
     })
   }
 
@@ -45,7 +79,7 @@ class App extends React.Component {
             <div className="carousel-title">{this.state.currentMovie} PHOTOS</div>
           </div>
           <div className="carousel-navbar">
-            <Navigation />
+            <Navigation total={this.state.carouselByFours} />
           </div>
         </div>
         <div className="carousel-bin">
