c6a2e1085db34558d73657e9e0a1ff880c9d7e30
j-gens <jgens2@gmail.com>
2019-10-14 12:11:06 -0700

    Add comments for future functionality

--- client/components/navigation.jsx
+++ client/components/navigation.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
 
+/*
+Currently:
+  > 'o' is a placeholder -- work will be done in a different pull request
+In future pull request:
+  > will update 'o' to css circles
+  > navigation circle will turn red to indicate 'place' in photo carousel
+*/
 const Navigation = (props) => (
   <div>
-    Navigation bar here
+    {props.total.map(ele =>
+      'o'
+    )}
   </div>
 );
 
