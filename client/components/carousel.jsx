42c4f495c294b94e123434e64f9aee880d6a5340
j-gens <jgens2@gmail.com>
2019-10-15 10:27:56 -0700

    Switch to styled components

--- client/components/carousel.jsx
+++ client/components/carousel.jsx
@@ -1,13 +1,26 @@
 import React from 'react';
+import styled from 'styled-components';
 import CarouselEntry from './carouselEntry.jsx';
 
 
+const CarouselLineWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;;
+
+  z-index: 1;
+  grid-column-start: 1;
+  grid-column-end: 29;
+  grid-row-start: 1;
+  grid-row-end: 4;
+`;
+
 const Carousel = (props) => (
-  <div className="carousel-line">
+  <CarouselLineWrapper>
     {props.carousel.map(entry =>
       <CarouselEntry entry={entry} />
     )}
-  </div>
+  </CarouselLineWrapper>
 )
 
 
