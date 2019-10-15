5260340bde5d3095ac3f0c48bac7930be8c93ca2
j-gens <jgens2@gmail.com>
2019-10-15 10:30:55 -0700

    Switch to styled components

--- client/components/carouselEntry.jsx
+++ client/components/carouselEntry.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
+import styled from 'styled-components';
 
 //image has on-hover movement -- may need to add state to this component
 
+const CarouselEntryWrapper = styled.div`
+  z-index: 2;
+`;
+
+const CarouselEntryImg = styled.img`
+  max-height: 167px;
+  max-width: 167px;
+`;
+
 const CarouselEntry = (props) => (
-  <div className="carousel-thumbnail">
-    <img src={props.entry.small_url} alt={props.entry._id} ></img>
-  </div>
+  <CarouselEntryWrapper>
+    <CarouselEntryImg src={props.entry.small_url} alt={props.entry._id} >
+    </CarouselEntryImg>
+  </CarouselEntryWrapper>
 )
 
 
