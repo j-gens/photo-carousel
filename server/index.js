8032182bdf8c88527f4894f9d15e0669306b598d
j-gens <jgens2@gmail.com>
2019-10-16 14:21:31 -0700

    Move routes to a separate module for testing purposes

--- server/index.js
+++ server/index.js
@@ -1,33 +1,5 @@
 const express = require('express');
-const db = require('../database/index.js')
-
-const app = express();
-app.use(express.static('public'));
-
-app.use(express.json());
-app.use(express.urlencoded({extended: true}))
-
-
-app.get('/api/imgsmall/:movieId', (req, res) => {
-  db.getCarousel(req.params.movieId, req.path, (err, data) => {
-    if (err) {
-      res.send(err);
-    } else {
-      res.send(data);
-    }
-  });
-});
-
-
-app.get('/api/imglarge/:movieId', (req, res) => {
-  db.getCarousel(req.params.movieId, req.path, (err, data) => {
-    if (err) {
-      res.send(err);
-    } else {
-      res.send(data);
-    }
-  });
-});
+const app = require('./app.js');
 
 
 //set up for future deployment:
@@ -35,4 +7,5 @@ let port = process.env.PORT;
 if (port == null || port == "") {
   port = 3100;
 }
+
 app.listen(port, () => console.log(`App is listening on port ${port}`));
