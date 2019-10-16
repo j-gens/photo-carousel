032184332a1c1e984a7891923c45437e8afc15e7
j-gens <jgens2@gmail.com>
2019-10-16 14:19:50 -0700

    Split server into two modules for testing purposes

---
+++ server/app.js
@@ -0,0 +1,33 @@
+const db = require('../database/index.js')
+const express = require('express');
+
+const app = express();
+
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}))
+
+
+app.get('/api/imgsmall/:movieId', (req, res) => {
+  db.getCarousel(req.params.movieId, req.path, (err, data) => {
+    if (err) {
+      res.send(err);
+    } else {
+      res.status(200).send(data);
+    }
+  });
+});
+
+
+app.get('/api/imglarge/:movieId', (req, res) => {
+  db.getCarousel(req.params.movieId, req.path, (err, data) => {
+    if (err) {
+      res.send(err);
+    } else {
+      res.send(data);
+    }
+  });
+});
+
+
+module.exports = app;
