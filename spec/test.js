3bc408aecfd21169f0947f9e39141a8b12293592
j-gens <jgens2@gmail.com>
2019-10-16 15:06:36 -0700

    Add additional tests for route handling

--- spec/test.js
+++ spec/test.js
@@ -4,19 +4,62 @@ const app = require('../server/app.js');
 const request = require('supertest');
 
 
-describe('server communication with database', () => {
-  let serverTest;
+describe('server responses', () => {
 
-  beforeEach(async (done) => {
-    serverTest = await request(app).get('/api/imgsmall/21210');
+  test('should respond with status 200 for movies in db', async () => {
+    for (let i = 0; i < 12; i++) {
+      let movieId = '2121' + i.toString();
+      let data = await request(app).get('/api/imgsmall/' + movieId);
 
-    done();
+      expect(data.statusCode).toBe(200);
+    }
   });
 
+  test('should respond with an array length 1 - 25', async () => {
+    for (let i = 0; i < 12; i++) {
+      let movieId = '2121' + i.toString();
+      let data = await request(app).get('/api/imgsmall/' + movieId);
 
-  test('should respond with 200', () => {
-    expect(serverTest.statusCode).toBe(200);
+      expect(Array.isArray(data.body)).toBeTruthy();
+      expect(data.body.length).toBeGreaterThanOrEqual(1);
+      expect(data.body.length).toBeLessThanOrEqual(25);
+    }
   });
+
+  test('should respond with status 200 for movies in db', async () => {
+    for (let i = 0; i < 12; i++) {
+      let movieId = '2121' + i.toString();
+      let data = await request(app).get('/api/imglarge/' + movieId);
+
+      expect(data.statusCode).toBe(200);
+    }
+  });
+
+  test('should respond with an array length 1 - 25', async () => {
+    for (let i = 0; i < 12; i++) {
+      let movieId = '2121' + i.toString();
+      let data = await request(app).get('/api/imglarge/' + movieId);
+
+      expect(Array.isArray(data.body)).toBeTruthy();
+      expect(data.body.length).toBeGreaterThanOrEqual(1);
+      expect(data.body.length).toBeLessThanOrEqual(25);
+    }
+  });
+
+  test('should return the same length array', async () => {
+    for (let i = 0; i < 12; i++) {
+      let movieId = '2121' + i.toString();
+      let data = await request(app).get('/api/imgsmall/' + movieId);
+      let dataTwo = await request(app).get('/api/imglarge/' + movieId);
+
+      expect(data.body.length).toEqual(dataTwo.body.length);
+    }
+  });
+
+  // test('should respond with 404 for movies not in db', async () => {
+  //   let data = await request(app).get('/api/imgsmall/00');
+  //   expect(data.statusCode).toBe(404);
+  // });
 
 });
 
