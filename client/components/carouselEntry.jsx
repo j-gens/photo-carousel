9e51d67402a1f7f93f2407d28360248acef9061d
j-gens <jgens2@gmail.com>
2019-10-17 12:53:42 -0700

    Add function so photo clicked is the one in the modal

--- client/components/carouselEntry.jsx
+++ client/components/carouselEntry.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Modal from 'react-modal';
+import axios from 'axios';
 import { CarouselEntryWrapper, CarouselEntryImg } from './stylesheet.jsx';
 
 
@@ -9,11 +10,42 @@ class CarouselEntry extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      modalIsOpen: false
+      modalIsOpen: false,
+      clickedPhoto: '',
+      currentPhoto: '',
+      largeCarousel: []
     }
 
+    this.handleClick = this.handleClick.bind(this);
+    this.findThePhoto = this.findThePhoto.bind(this);
     this.openModal = this.openModal.bind(this);
     this.closeModal = this.closeModal.bind(this);
+  }
+
+
+  handleClick() {
+    this.setState({clickedPhoto: this.props.entry._id});
+
+    axios.get(`/api/imglarge/${this.props.entry.movie.id}`)
+    .then(response => {
+      console.log(response);
+      this.setState({largeCarousel: response.data})
+    })
+    .finally(() => {
+      this.findThePhoto();
+      this.openModal();
+    })
+    .catch(error => {
+      console.log(error);
+    })
+  }
+
+  findThePhoto() {
+    for (let i = 0; i < this.state.largeCarousel.length; i++) {
+      if (this.state.largeCarousel[i]._id === this.state.clickedPhoto) {
+        this.setState({currentPhoto: this.state.largeCarousel[i].large_url});
+      }
+    }
   }
 
   openModal() {
@@ -32,16 +64,20 @@ class CarouselEntry extends React.Component {
         <Modal
         isOpen={this.state.modalIsOpen}
         onRequestClose={this.closeModal}
-        contentLabel="image modal">
-          <button onClick={this.closeModal}> X </button>
-          <img src={this.props.entry.small_url}></img>
+        contentLabel="image carousel modal">
+          <div>
+            <button onClick={this.closeModal}> X </button>
+          </div>
+          <div>
+            <img src={this.state.currentPhoto}></img>
+          </div>
        </Modal>
       )
     }
 
     return (
       <CarouselEntryWrapper>
-        <CarouselEntryImg src={this.props.entry.small_url} alt={this.props.entry._id} onClick={this.openModal}>
+        <CarouselEntryImg src={this.props.entry.small_url} alt={this.props.entry._id} onClick={this.handleClick}>
         </CarouselEntryImg>
         {modalDisplay}
       </CarouselEntryWrapper>
