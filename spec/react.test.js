95856cab3d15d0cbbadb0d8489bccaee24ca725e
j-gens <jgens2@gmail.com>
2019-10-17 10:49:08 -0700

    Add tests for button clicks

--- spec/react.test.js
+++ spec/react.test.js
@@ -36,6 +36,30 @@ describe('App component', () => {
     expect(axios.get).toHaveBeenCalledTimes(1);
   })
 
+  it('simulates right button clicks', () => {
+    const images = [{_id: 1, small_url: 'https://hrr41-fec-krillin-imgs.s3-us-west-1.amazonaws.com/small0.jpg', movie: {id: 21210, title: 'Detective Pikachu'}}];
+    const resp = {data: images};
+    axios.get.mockResolvedValue(resp);
+
+    const mockOnClick = jest.fn();
+    const wrapper = shallow(<stylesheet__CarouselButtonRight value=">" onClick={mockOnClick}> &gt; </stylesheet__CarouselButtonRight>);
+
+    wrapper.find('stylesheet__CarouselButtonRight').simulate('click');
+    expect(mockOnClick.mock.calls.length).toEqual(1);
+  })
+
+  it('simulates left button clicks', () => {
+    const images = [{_id: 1, small_url: 'https://hrr41-fec-krillin-imgs.s3-us-west-1.amazonaws.com/small0.jpg', movie: {id: 21210, title: 'Detective Pikachu'}}];
+    const resp = {data: images};
+    axios.get.mockResolvedValue(resp);
+
+    const mockOnClick = jest.fn();
+    const wrapper = shallow(<stylesheet__CarouselButtonLeft value=">" onClick={mockOnClick}> &gt; </stylesheet__CarouselButtonLeft>);
+
+    wrapper.find('stylesheet__CarouselButtonLeft').simulate('click');
+    expect(mockOnClick.mock.calls.length).toEqual(1);
+  })
+
 })
 
 
