ca55afe09930257b7f6e7e9b4baf20a47acf1461
j-gens <jgens2@gmail.com>
2019-10-17 11:19:47 -0700

    Change carouselEntry to a class component

--- client/components/carouselEntry.jsx
+++ client/components/carouselEntry.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
+import Modal from 'react-modal';
 import { CarouselEntryWrapper, CarouselEntryImg } from './stylesheet.jsx';
 
 
-const CarouselEntry = (props) => (
-  <CarouselEntryWrapper>
-    <CarouselEntryImg src={props.entry.small_url} alt={props.entry._id} >
-    </CarouselEntryImg>
-  </CarouselEntryWrapper>
-)
+class CarouselEntry extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+      <CarouselEntryWrapper>
+        <CarouselEntryImg src={props.entry.small_url} alt={props.entry._id} >
+        </CarouselEntryImg>
+      </CarouselEntryWrapper>
+    );
+  }
+}
 
 
 export default CarouselEntry;
