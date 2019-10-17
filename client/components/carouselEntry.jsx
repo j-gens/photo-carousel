79d254599446316f4c7fce92e1fdd8b3f0c97cf1
j-gens <jgens2@gmail.com>
2019-10-17 11:40:53 -0700

    Add basic modal on photo click (no styling yet)

--- client/components/carouselEntry.jsx
+++ client/components/carouselEntry.jsx
@@ -3,19 +3,47 @@ import Modal from 'react-modal';
 import { CarouselEntryWrapper, CarouselEntryImg } from './stylesheet.jsx';
 
 
+Modal.setAppElement('#imgcarousel');
+
 class CarouselEntry extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      modalIsOpen: false
     }
+
+    this.openModal = this.openModal.bind(this);
+    this.closeModal = this.closeModal.bind(this);
+  }
+
+  openModal() {
+    this.setState({modalIsOpen: true});
+  }
+
+  closeModal() {
+    this.setState({modalIsOpen: false});
   }
 
   render() {
+    let modalDisplay;
+
+    if (this.state.modalIsOpen) {
+      modalDisplay = (
+        <Modal
+        isOpen={this.state.modalIsOpen}
+        onRequestClose={this.closeModal}
+        contentLabel="image modal">
+          <button onClick={this.closeModal}> X </button>
+          <img src={this.props.entry.small_url}></img>
+       </Modal>
+      )
+    }
+
     return (
       <CarouselEntryWrapper>
-        <CarouselEntryImg src={props.entry.small_url} alt={props.entry._id} >
+        <CarouselEntryImg src={this.props.entry.small_url} alt={this.props.entry._id} onClick={this.openModal}>
         </CarouselEntryImg>
+        {modalDisplay}
       </CarouselEntryWrapper>
     );
   }
