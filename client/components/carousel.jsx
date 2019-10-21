8fa6c75ced97b5c1054b0bcceed109f9825ca306
j-gens <jgens2@gmail.com>
2019-10-20 19:44:34 -0700

    Pass along image arrays as props

--- client/components/carousel.jsx
+++ client/components/carousel.jsx
@@ -5,8 +5,14 @@ import { CarouselLineWrapper } from './stylesheet.jsx';
 
 const Carousel = (props) => (
   <CarouselLineWrapper>
+    {props.lagger.map((lagger) =>
+      <CarouselEntry entry={lagger} animate={props.animate} />
+    )}
     {props.carousel.map((entry) =>
       <CarouselEntry entry={entry} length={props.length} animate={props.animate} />
+    )}
+    {props.leader.map((leader) =>
+      <CarouselEntry entry={leader} animate={props.animate} />
     )}
   </CarouselLineWrapper>
 )
