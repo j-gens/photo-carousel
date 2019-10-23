f62e341bfd21f2f2ee844be46fe27a975b97b1da
j-gens <jgens2@gmail.com>
2019-10-23 08:27:49 -0700

    Add key to all carouselEntry components

--- client/components/carousel.jsx
+++ client/components/carousel.jsx
@@ -5,13 +5,13 @@ import { CarouselLineWrapper } from './stylesheet.jsx';
 const Carousel = (props) => (
   <CarouselLineWrapper>
     {props.lagger.map((lagger) =>
-      <CarouselEntry entry={lagger} animate={props.animate} />
+      <CarouselEntry entry={lagger} key={lagger._id} animate={props.animate} />
     )}
     {props.carousel.map((entry) =>
-      <CarouselEntry entry={entry} length={props.length} animate={props.animate} />
+      <CarouselEntry entry={entry} key={entry._id} length={props.length} animate={props.animate} />
     )}
     {props.leader.map((leader) =>
-      <CarouselEntry entry={leader} animate={props.animate} />
+      <CarouselEntry entry={leader} key={leader._id} animate={props.animate} />
     )}
   </CarouselLineWrapper>
 )
