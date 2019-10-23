950eb243e355053771986e062f7ad9e1df631c58
j-gens <jgens2@gmail.com>
2019-10-23 10:13:25 -0700

    Update webpack for styled-components

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
   },
   resolve: {
     alias: {
-      "styled-components": path.resolve(__dirname, "node_modules", "styled-components")
+      "styled-components$": path.resolve(__dirname, "node_modules", "styled-components")
     }
   },
   watch: true
